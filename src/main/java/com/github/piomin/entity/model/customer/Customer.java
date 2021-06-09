943a199f8213741c2f602a687f3622c771ed3f61
piomin <piotr.minkowski@gmail.com>
2021-06-09 15:11:46 +0200

    Order + Customer

---
+++ src/main/java/com/github/piomin/entity/model/customer/Customer.java
@@ -0,0 +1,24 @@
+package com.github.piomin.entity.model.customer;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private int amountAvailable;
+    private int amountReserved;
+}
---
+++ src/main/java/com/github/piomin/entity/model/order/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.github.piomin.entity.model.order;
+
+public enum OrderStatus {
+    NEW, REJECTED, CONFIRMED, IN_PROGRESS;
+}
